eb9c4c728da2409409b6216b4883e15816d5f96d
Thibishaan <thibishaan@gmail.com>
2026-03-29 20:37:44 +1000

    Added unit tests for ParentNode class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -33,6 +33,23 @@ class TestTextNode(unittest.TestCase):
     def test_leafnode_3(self):
         node = LeafNode(tag="p", value="Hello World", props={"href": "https://github.com"})
         self.assertEqual(node.to_html(), '<p href="https://github.com">Hello World</p>')
+
+    def test_parent_to_html_1(self):
+        child_node = LeafNode(tag='b', value='HI')
+        parent_node = ParentNode(tag='p', children=[child_node])
+        self.assertEqual(parent_node.to_html(), '<p><b>HI</b></p>')
+        
+    def test_parent_to_html_2(self):
+        child_node = LeafNode(tag='b', value='HI')
+        child_node2 = LeafNode(tag='b', value='HI')
+        parent_node = ParentNode(tag='p', children=[child_node, child_node2])
+        self.assertEqual(parent_node.to_html(), '<p><b>HI</b><b>HI</b></p>')
+
+    def test_parent_to_html_3(self):
+        grandchild_node = LeafNode(tag='b', value='HI')
+        child_node = ParentNode(tag='p', children=[grandchild_node])
+        parent_node = ParentNode(tag='div', children=[child_node])
+        self.assertEqual(parent_node.to_html(), '<div><p><b>HI</b></p></div>')
 
 if __name__ == "__main__":
     unittest.main()
