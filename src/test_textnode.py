65017432bb90f6fe6111788271c37637717b6fb2
Equinox2654 <thibishaan@gmail.com>
2026-04-12 17:38:40 +1000

    Added tests for text to textnode function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, split_nodes_image, split_nodes_link
+from textnode import TextNode, TextType, split_nodes_image, split_nodes_link, text_to_textnodes
 
 
 class TestTextNode(unittest.TestCase):
@@ -111,6 +111,45 @@ class TestTextNode(unittest.TestCase):
             ]
         )
 
+    def test_text_to_node_1(self):
+        text = "This **test** has _multiple_ **types** of `words` and a [link](https://link.com) plus an image ![image](https://image.com)"
+        node_list = text_to_textnodes(text)
+        self.assertListEqual(
+            node_list,
+            [
+                TextNode("This ", TextType.TEXT),
+                TextNode("test", TextType.BOLD),
+                TextNode(" has ", TextType.TEXT),
+                TextNode("multiple", TextType.ITALIC),
+                TextNode(" ", TextType.TEXT),
+                TextNode("types", TextType.BOLD),
+                TextNode(" of ", TextType.TEXT),
+                TextNode("words", TextType.CODE),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://link.com"),
+                TextNode(" plus an image ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://image.com")
+            ]
+        )
+
+    def test_text_to_node_2(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            nodes,
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ]
+        )
 
 if __name__ == "__main__":
     unittest.main()
